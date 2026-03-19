3d2aebd2e0d83ad54b2ea3ebcbb80e30a786abbc
momonala <mnalavadi@gmail.com>
2026-03-19 22:21:40 +0100

    add app docs and cleanup

--- js/resume.js
+++ js/resume.js
@@ -1,24 +1,42 @@
 (function($) {
   "use strict"; // Start of use strict
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+  var SCROLL_ANIMATION_DURATION_MS = 1000;
+
+  function handleSmoothScroll(event) {
+    var targetHash = this.hash;
+    if (!targetHash) {
+      return;
+    }
+
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') &&
+        location.hostname === this.hostname) {
+      var target = $(targetHash);
+      target = target.length ? target : $('[name=' + targetHash.slice(1) + ']');
       if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top)
-        }, 1000, "easeInOutExpo");
-        return false;
+        event.preventDefault();
+        event.stopPropagation();
+
+        $('html, body').animate(
+          {
+            scrollTop: (target.offset().top)
+          },
+          SCROLL_ANIMATION_DURATION_MS,
+          "easeInOutExpo"
+        );
       }
     }
-  });
+  }
+
+  function handleNavCollapse() {
+    $('.navbar-collapse').collapse('hide');
+  }
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').on('click', handleSmoothScroll);
 
   // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
+  $('.js-scroll-trigger').on('click', handleNavCollapse);
 
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
